78a30bbb714514eadabfe15eafcbf8761f4bfb6a
willjgh <williamjgh2000@gmail.com>
2026-08-24 19:17:40 +0100

    Setup run for real data fixed real library size

--- batch-run-cleanup.py
+++ batch-run-cleanup.py
@@ -7,7 +7,11 @@ data_list = [
 ]
 
 model_list = [
-    {"model_name": "NB-16-2-1-400-const", "gene_likelihood": "nb", "n_hidden": 16, "n_latent": 2, "n_layers": 1, "lib": "const"}
+    {"model_name": "NB-128-2-1-400-real", "gene_likelihood": "nb", "n_hidden": 128, "n_latent": 2, "n_layers": 1, "lib": "real"},
+    {"model_name": "NB-4-2-1-400-real", "gene_likelihood": "nb", "n_hidden": 4, "n_latent": 2, "n_layers": 1, "lib": "real"},
+    {"model_name": "Poi-128-2-1-400-real", "gene_likelihood": "poisson", "n_hidden": 128, "n_latent": 2, "n_layers": 1, "lib": "real"},
+    {"model_name": "Poi-16-2-1-400-real", "gene_likelihood": "poisson", "n_hidden": 16, "n_latent": 2, "n_layers": 1, "lib": "real"},
+    {"model_name": "Poi-4-2-1-400-real", "gene_likelihood": "poisson", "n_hidden": 4, "n_latent": 2, "n_layers": 1, "lib": "real"},
 ]
 
 # ------------------------------------------------------------------------------
