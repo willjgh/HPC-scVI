5e2ea18ed4955b3c22e996d45890c7ab374d5c03
willjgh <williamjgh2000@gmail.com>
2026-08-24 18:48:58 +0100

    Run test that was overwritten

--- batch-run-cleanup.py
+++ batch-run-cleanup.py
@@ -3,22 +3,11 @@
 # ------------------------------------------------------------------------------
 
 data_list = [
-    "real-data",
-    "lognormal-nb",
-    "lognormal-poi",
-    "copula-nb",
-    "copula-poi",
-    "indep-nb",
-    "indep-poi"
+    "real-data"
 ]
 
 model_list = [
-    {"model_name": "Poi-128-2-1-400-pad", "gene_likelihood": "poisson", "n_hidden": 128, "n_latent": 2, "n_layers": 1, "lib": "pad"},
-    {"model_name": "Poi-16-2-1-400-const", "gene_likelihood": "poisson", "n_hidden": 16, "n_latent": 2, "n_layers": 1, "lib": "pad"},
-    {"model_name": "Poi-4-2-1-400-real", "gene_likelihood": "poisson", "n_hidden": 4, "n_latent": 2, "n_layers": 1, "lib": "pad"},
-    {"model_name": "NB-128-2-1-400-pad", "gene_likelihood": "nb", "n_hidden": 128, "n_latent": 2, "n_layers": 1, "lib": "pad"},
-    {"model_name": "NB-16-2-1-400-const", "gene_likelihood": "nb", "n_hidden": 16, "n_latent": 2, "n_layers": 1, "lib": "pad"},
-    {"model_name": "NB-4-2-1-400-real", "gene_likelihood": "nb", "n_hidden": 4, "n_latent": 2, "n_layers": 1, "lib": "pad"}
+    {"model_name": "NB-16-2-1-400-const", "gene_likelihood": "poisson", "n_hidden": 16, "n_latent": 2, "n_layers": 1, "lib": "const"}
 ]
 
 # ------------------------------------------------------------------------------
--- conda-batch-run.sh
+++ conda-batch-run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #PBS -l walltime=01:00:00
 #PBS -l select=1:ncpus=1:mem=4gb
-#PBS -J 1-1260
+#PBS -J 1-1
 
 eval "$(~/miniforge3/bin/conda shell.bash hook)"
 conda activate scVIpy311
